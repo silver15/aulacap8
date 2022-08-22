48a5fcfce4503c58ee760d43f0f6c37f1d409d31
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-08-22 19:49:48 -0300

    Frontend cap 7

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import './assets/styles/custom.scss';
+import './App.css';
+
+import Routes from 'Route';
+
+function App() {
+  return <Routes />;
+}
+
+export default App;
---
+++ frontend/src/components/buttonIcon/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css';
+import { ReactComponent as ArrowIcon } from 'assets/image/seta.svg';
+
+const ButtonIcon = () => {
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>INICIE AGORA A SUA BUSCA</h6>
+      </button>
+
+      <div className="btn-icon-container">
+        <ArrowIcon />
+      </div>
+    </div>
+  );
+};
+
+export default ButtonIcon;
