6f7f1420a5308cb554664f5fa527698d8e45606f
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-08-28 11:28:49 -0300

    productDetails responsive

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -8,36 +8,36 @@ const ProductDetails = () => {
     <div className="product-details-container">
       <div className="product-details-card">
         <div className="goback-container">
-        <ArrowIcon />
-        <h2>VOLTAR</h2>
-      </div>
-      <div className="row">
-        <div className="col-xl-6">
-          <div className="img-container">
-            <img
-              src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-              alt="Nome do produto"
-            />
-          </div>
-          <div className="name-price-container">
-            <h1>Nome do produto</h1>
-            <ProductPrice price={2345.67} />
-          </div>
+          <ArrowIcon />
+          <h2>VOLTAR</h2>
         </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Nome do produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
 
-        <div className="col-xl-6">
-          <div className="description-container">
-            <h2>Descrição do Produto</h2>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Accusantium, praesentium.
-            </p>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do Produto</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Accusantium, praesentium.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
-}
+};
 
 export default ProductDetails;
