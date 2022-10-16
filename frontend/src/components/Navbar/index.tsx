37f95741f06a321cccde3230e9ab994c72f00206
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-10-16 16:42:10 -0300

    organazing utils

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,10 +1,11 @@
 import './styles.css';
 import 'bootstrap/js/src/collapse.js';
 import { Link, NavLink } from 'react-router-dom';
-import { getTokenData, isAuthenticated, removeAuthData} from 'util/requests';
 import history from 'util/history';
 import { AuthContext } from 'AuthContext';
 import { useContext, useEffect } from 'react';
+import { getTokenData, isAuthenticated } from 'util/auth';
+import { removeAuthData } from 'util/storage';
 
 
 
